b8f8c9b6dde254f8689a44eca804f89f7f35d5a7
Ray <ray.masiclat@gmail.com>
2018-12-22 14:08:50 -0700

    URL Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/raypage",
+  pathPrefix: "/Raaaaaaay.github.io",
   siteMetadata: {
     title: `Ray Masiclat`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
